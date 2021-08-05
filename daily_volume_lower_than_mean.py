89cd2527ce21f4cbb1df0820d4d6b61ec1210158
heroheat <hiro.komura@gmail.com>
2021-08-05 09:41:18 +0900

    Delete daily_volume_lower_than_mean.py

--- daily_volume_lower_than_mean.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('./AAPL.csv')
-
-median_daily_volume = df["Volume"].median()
-print("The arithmetic mean of the daily volume in this dataset is " + str(median_daily_volume))
-print("The number of days where the daily volume is below the arithmetic mean is " + str(df[df["Volume"] < median_daily_volume].shape[0]))
